e3f033833486a88fe5c68dca85d386a0d97801dd
Caroline <caroline.portilla@gmail.com>
2020-07-04 22:31:26 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Entry from "./pages/Entry";
+import Bio from "./pages/Bio";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+//import Navbar from "./components/Navbar";
+//import Footer from "./components/Footer";
+import Container from "./components/Container";
+function App() {
+  return(
+    <Router>
+      
+          <Route exact path="/" component={Entry} />
+          <Route exact path="/entry" component={Entry} />
+          <Route exact path="/bio" component={Bio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/pages/Entry.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import Container from "..components/Container";
+
+function Entry() {
+    return (
+        null
+    )
+}
+
+export default Entry;
