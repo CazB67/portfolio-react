50aa8c514b6564db2f27cdd49076731aa723213f
Caroline Bates <caroline.portilla@gmail.com>
2024-04-22 13:26:29 +0800

    update css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,7 +7,7 @@ function Card (props) {
     return (
                 <div className="card-body mb-3">
                     <h2 className="card-header about-me slide mb-3">About Me</h2>
-                        <img src={me} className="img-fluid float-left pr-3" alt="Caroline Bates"/>
+                        <img src={me} className="img-fluid float-left avatar" alt="Caroline Bates"/>
                         <p className="card-text">{props.cardtext}</p>
                         <p className="card-text">{props.cardtext2}</p>
                         <p className="card-text mb-4">{props.cardtext3}</p>
